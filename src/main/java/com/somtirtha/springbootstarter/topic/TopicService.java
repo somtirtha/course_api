92e6cb2d2c1b41d1d5cea9bb7daf7e116764356f
Somtirtha Roy <somtirtha.roy@gmail.com>
2017-02-14 10:25:28 -0700

    Added update topics, delete topics to Topics Controller and Service

--- src/main/java/com/somtirtha/springbootstarter/topic/TopicService.java
+++ src/main/java/com/somtirtha/springbootstarter/topic/TopicService.java
@@ -1,25 +1,44 @@
 package com.somtirtha.springbootstarter.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class TopicService {
-	private List<Topic> topics = Arrays.asList(
-			new Topic("spring", "Spring Framework", "Spring Framework Description"),
-			new Topic("java", "Core Java", "Core Java Description"),
-			new Topic("javascript", "Javascript", "Javascript  Description") 
-			
-			);
-	
+	private List<Topic> topics = new ArrayList<>(
+			Arrays.asList(new Topic("spring", "Spring Framework", "Spring Framework Description"),
+					new Topic("java", "Core Java", "Core Java Description"),
+					new Topic("javascript", "Javascript", "Javascript  Description")
+
+			));
+
 	public List<Topic> getAllTopics() {
 		return topics;
 	}
-	
+
 	public Topic getTopic(String id) {
 		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
 	}
+
+	void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+
+	void updateTopic(Topic topic, String id) {
+		for (int i = 0; i < topics.size(); ++i) {
+			Topic t = topics.get(i);
+			if (t.getId().equals(id)) {
+				topics.set(i, topic);
+				return;
+			}
+		}
+	}
+
+	 void deleteTopic(String id) {
+		 topics.removeIf(t -> t.getId().equals(id));
+	 }
 }
- 
