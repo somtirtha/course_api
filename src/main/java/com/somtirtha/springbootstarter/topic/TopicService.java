076f94b137fcab1a90f5885435a079a1db5aa902
Somtirtha Roy <somtirtha.roy@gmail.com>
2017-02-15 22:31:29 -0700

    Removed comments and added message referene in place of lambda function in topic service

--- src/main/java/com/somtirtha/springbootstarter/topic/TopicService.java
+++ src/main/java/com/somtirtha/springbootstarter/topic/TopicService.java
@@ -14,44 +14,25 @@ public class TopicService {
 	@Autowired
 	private TopicRepository topicRepository;
 	
-//	private List<Topic> topics = new ArrayList<>(
-//			Arrays.asList(new Topic("spring", "Spring Framework", "Spring Framework Description"),
-//					new Topic("java", "Core Java", "Core Java Description"),
-//					new Topic("javascript", "Javascript", "Javascript  Description")
-//
-//			));
- 
 	public List<Topic> getAllTopics() {
-//		return topics;
 		List<Topic> topics = new ArrayList<>();
-//		topicRepository.findAll().forEach(topics::add);
-		topicRepository.findAll().forEach(t -> topics.add(t));
+		topicRepository.findAll().forEach(topics::add);
 		return topics;
 	}
 
 	public Topic getTopic(String id) {
-//		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
 		return topicRepository.findOne(id);
 	}
 
 	void addTopic(Topic topic) {
-//		topics.add(topic);
 		topicRepository.save(topic);
 	}
 
 	void updateTopic(Topic topic, String id) {
-//		for (int i = 0; i < topics.size(); ++i) {
-//			Topic t = topics.get(i);
-//			if (t.getId().equals(id)) {
-//				topics.set(i, topic);
-//				return;
-//			}
-//		}
 		topicRepository.save(topic);
 	}
 
 	 void deleteTopic(String id) {
-//		 topics.removeIf(t -> t.getId().equals(id));
 		 topicRepository.delete(id);
 	 }
 }
