ed3d46ecebb0b8abcce10e8d3cf9c965de05e20a
Somtirtha Roy <somtirtha.roy@gmail.com>
2017-02-15 22:29:20 -0700

    Added data layer to the app, TopicRepository interface extending CrudRepository, changes all methods in service to use the interface's crud methods

--- src/main/java/com/somtirtha/springbootstarter/topic/Topic.java
+++ src/main/java/com/somtirtha/springbootstarter/topic/Topic.java
@@ -1,6 +1,12 @@
 package com.somtirtha.springbootstarter.topic;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Topic {
+	
+	@Id
 	private String id;
 	private String name;
 	private String description;
